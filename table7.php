b0446d4b393b72ba7b843094cb8fea9896d5e2b7
Apisarapusomboon <98233747+Apisarapusomboon@users.noreply.github.com>
2022-03-02 14:14:59 +0700

    Create table7.php

---
+++ table7.php
@@ -0,0 +1,86 @@
+<html>
+     <head>
+       <title>การสร้างตาราง</title>
+     </head>
+     <body>
+       <table border="1">
+         <tr>
+           <th>ลำดับ</th>
+           <th>ชื่อ</th>
+           <th>รูปภาพ</th>
+           <th>Twitter/Instragram</th>
+         </tr>
+         <tr>
+           <td>1</td>
+           <td>ชา อึนอู</td>
+           <td><img src="https://korism.com/_upload/news/2018/10/125043/15402097921.jpg" width="45px"></td>
+           <td><a href="https://instagram.com/eunwo.o_c?utm_medium=copy_link">Instragram</a></td>
+         </tr>
+       </table>
+     </body>
+   </html>
+           <table border="2">
+         <tr>
+           <th>ลำดับ</th>
+           <th>ชื่อ</th>
+           <th>รูปภาพ</th>
+           <th>Twitter/Instragram</th>
+         </tr>
+         <tr>
+           <td>2</td>
+           <td>คิม โดยอง</td>
+           <td><img src="https://f.ptcdn.info/294/073/000/qs7b661nsWToj9afrk4-o.jpg" width="45px"></td>
+           <td><a href="https://instagram.com/do0_nct?utm_medium=copy_link">Instragram</a></td>
+         </tr>
+       </table>
+     </body>
+   </html>
+
+          <table border="3">
+         <tr>
+           <th>ลำดับ</th>
+           <th>ชื่อ</th>
+           <th>รูปภาพ</th>
+           <th>Twitter/Instragram</th>
+         </tr>
+         <tr>
+          <td>3</td>
+           <td>ลิซ่า</td>
+           <td><img src="https://www.dailynews.co.th/wp-content/uploads/2021/10/%E0%B8%81%E0%B8%94%E0%B8%AB%E0%B8%94%E0%B8%81%E0%B8%AB%E0%B8%94.jpg" width="45px"></td>
+           <td><a href="instagram.com/lalalalisa_m?utm_medium=copy_lin">Instragram</a></td>
+         </tr>
+       </table>
+     </body>
+   </html>
+          <table border="4">
+         <tr>
+           <th>ลำดับ</th>
+           <th>ชื่อ</th>
+           <th>รูปภาพ</th>
+           <th>Twitter/Instragram</th>
+         </tr>
+         <tr>
+          <td>4</td>
+           <td>เจนนี่</td>
+           <td><img src="https://board.postjung.com/1330622#" width="45px"></td>
+           <td><a href="https://instagram.com/jennierubyjane?utm_medium=copy_link">Instragram</a></td>
+         </tr>
+       </table>
+     </body>
+   </html>
+          <table border="5">
+         <tr>
+           <th>ลำดับ</th>
+           <th>ชื่อ</th>
+           <th>รูปภาพ</th>
+           <th>Twitter/Instragram</th>
+         </tr>
+         <tr>
+          <td>5</td>
+           <td>จีซู</td>
+           <td><img src="https://s.isanook.com/mv/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL212LzAvdWQvMjIvMTEyOTA1L3VudGl0bGVkLTEuanBn.jpg" width="45px"></td>
+           <td><a href="https://instagram.com/sooyaaa__?utm_medium=copy_link">Instragram</a></td>
+         </tr>
+       </table>
+     </body>
+   </html>
