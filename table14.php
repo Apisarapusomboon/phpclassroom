55434af92c60389e3ad2a5a1ecd88a9db539a055
Apisarapusomboon <98233747+Apisarapusomboon@users.noreply.github.com>
2022-03-02 14:46:55 +0700

    Create table14.php

---
+++ table14.php
@@ -0,0 +1,45 @@
+<html>
+        <head>
+          <title>การสร้างตาราง</title>
+        </head>
+        <body>
+          <table border="1">
+            <tr>
+              <th>ลำดับ</th>
+              <th>ชื่อ</th>
+              <th>รูปภาพ</th>
+              <th>Twitter/Instragram</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>ชา อึนอู</td>
+              <td><img src="https://korism.com/_upload/news/2018/10/125043/15402097921.jpg" width="45px"></td>
+              <td><a href="https://instagram.com/eunwo.o_c?utm_medium=copy_link">Instragram</a></td>
+            
+              <td>2</td>
+              <td>คิม โดยอง</td>
+              <td><img src="https://s.isanook.com/mv/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL212LzAvdWQvMjAvMTA0NzI1L3VudGl0bGVkLTEuanBn.jpg" width="45px"></td>
+              <td><a href="https://instagram.com/do0_nct?utm_medium=copy_link">Instragram</a></td>
+            </tr>
+              <tr>
+             <td>3</td>
+              <td>ลิซ่า</td>
+              <td><img src="https://www.dailynews.co.th/wp-content/uploads/2021/10/%E0%B8%81%E0%B8%94%E0%B8%AB%E0%B8%94%E0%B8%81%E0%B8%AB%E0%B8%94.jpg" width="45px"></td>
+              <td><a href="instagram.com/lalalalisa_m?utm_medium=copy_lin">Instragram</a></td>
+            </tr>
+              <tr>
+             <td>4</td>
+              <td>เจนนี่</td>
+              <td><img src="https://www.tnnthailand.com/static/images/d2c04496-6731-4e16-9e35-f14d7031b7b0.jpg" width="45px"></td>
+              <td><a href="https://instagram.com/jennierubyjane?utm_medium=copy_link">Instragram</a></td>
+            </tr>
+              <tr>
+             <td>5</td>
+              <td>จีซู</td>
+              <td><img src="https://s.isanook.com/mv/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL212LzAvdWQvMjIvMTEyOTA1L3VudGl0bGVkLTEuanBn.jpg" width="45px"></td>
+              <td><a href="https://instagram.com/sooyaaa__?utm_medium=copy_link">Instragram</a></td>
+            </tr>
+            </table>
+        </body>
+      </html>
+            
