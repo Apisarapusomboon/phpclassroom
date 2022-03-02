fd76f3e25e9b800a1981f26f1008c728de0e15ef
Apisarapusomboon <98233747+Apisarapusomboon@users.noreply.github.com>
2022-03-02 14:02:39 +0700

    Create table6.php

---
+++ table6.php
@@ -0,0 +1,86 @@
+<html>
+    <head>
+      <title>การสร้างตาราง</title>
+    </head>
+    <body>
+      <table border="1">
+        <tr>
+          <th>ลำดับ</th>
+          <th>ชื่อ</th>
+          <th>รูปภาพ</th>
+          <th>Twitter/Instragram</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>ชา อึนอู</td>
+          <td><img src="https://korism.com/_upload/news/2018/10/125043/15402097921.jpg" width="45px"></td>
+          <td><a href="https://instagram.com/eunwo.o_c?utm_medium=copy_link">Instragram</a></td>
+        </tr>
+      </table>
+    </body>
+  </html>
+          <table border="2">
+        <tr>
+          <th>ลำดับ</th>
+          <th>ชื่อ</th>
+          <th>รูปภาพ</th>
+          <th>Twitter/Instragram</th>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>คิม โดยอง</td>
+          <td><img src="https://pin.it/2vwsGt5" width="45px"></td>
+          <td><a href="https://instagram.com/do0_nct?utm_medium=copy_link">Instragram</a></td>
+        </tr>
+      </table>
+    </body>
+  </html>
+         
+         <table border="3">
+        <tr>
+          <th>ลำดับ</th>
+          <th>ชื่อ</th>
+          <th>รูปภาพ</th>
+          <th>Twitter/Instragram</th>
+        </tr>
+        <tr>
+         <td>3</td>
+          <td>ลิซ่า</td>
+          <td><img src="https://pin.it/4tMhRJp" width="45px"></td>
+          <td><a href="instagram.com/lalalalisa_m?utm_medium=copy_lin">Instragram</a></td>
+        </tr>
+      </table>
+    </body>
+  </html>
+         <table border="4">
+        <tr>
+          <th>ลำดับ</th>
+          <th>ชื่อ</th>
+          <th>รูปภาพ</th>
+          <th>Twitter/Instragram</th>
+        </tr>
+        <tr>
+         <td>4</td>
+          <td>เจนนี่</td>
+          <td><img src="https://pin.it/QTKQzll" width="45px"></td>
+          <td><a href="https://instagram.com/jennierubyjane?utm_medium=copy_link">Instragram</a></td>
+        </tr>
+      </table>
+    </body>
+  </html>
+         <table border="5">
+        <tr>
+          <th>ลำดับ</th>
+          <th>ชื่อ</th>
+          <th>รูปภาพ</th>
+          <th>Twitter/Instragram</th>
+        </tr>
+        <tr>
+         <td>5</td>
+          <td>จีซู</td>
+          <td><img src="https://pin.it/1kDh0Ar" width="45px"></td>
+          <td><a href="https://instagram.com/sooyaaa__?utm_medium=copy_link">Instragram</a></td>
+        </tr>
+      </table>
+    </body>
+  </html>
