c36d99b9a180bc2d7fca26b6ff4dd0f67ae13ecc
Apisarapusomboon <98233747+Apisarapusomboon@users.noreply.github.com>
2022-03-02 13:50:00 +0700

    Create table4.php

---
+++ table4.php
@@ -0,0 +1,53 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/Instragram</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>ชา อึนอู</td>
+        <td><img src="https://korism.com/_upload/news/2018/10/125043/15402097921.jpg" width="45px"></td>
+        <td><a href="https://instagram.com/eunwo.o_c?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
+        <td>2</td>
+        <td>คิม โดยอง</td>
+        <td><img src="https://images.app.goo.gl/8hRmzLc4JL5u4XLp8" width="45px"></td>
+        <td><a href="https://instagram.com/do0_nct?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
+        <td>3</td>
+        <td>ลิซ่า</td>
+        <td><img src="https://images.app.goo.gl/8u3D1jh9Uto2MGj7A" width="45px"></td>
+        <td><a href="instagram.com/lalalalisa_m?utm_medium=copy_lin">Instragram</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
+        <td>4</td>
+        <td>เจนนี่</td>
+        <td><img src="https://images.app.goo.gl/mCWk17PCUeRqVonR7" width="45px"></td>
+        <td><a href="https://instagram.com/jennierubyjane?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
+        <td>5</td>
+        <td>จีซู</td>
+        <td><img src="https://images.app.goo.gl/dtbTw9m1D1tuzB6u7" width="45px"></td>
+        <td><a href="https://instagram.com/sooyaaa__?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
